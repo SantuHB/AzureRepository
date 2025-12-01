3e9f301a3ad0ab45427604ab0cca9e1384eb93e4
Santhosh H B <santhosh.proxy@gmail.com>
2025-12-01 12:08:50 +0530

    Code Committed

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureDeploy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("Details")]
+        public IActionResult GetDetails()
+        {
+            return Ok(new string[] {"Amazon","Flipkart"});
+        }
+    }
+}
